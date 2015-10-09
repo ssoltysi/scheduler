579cd543783180560bc0dc9b7f049bf1076d1944
ssoltysi <seumas.soltysik@gmail.com>
2015-10-09 01:00:05 -0400

    committing

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import driver
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template("my_form.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+
+    date = request.form['date']
+    time = request.form['time']
+    rounds = request.form['rounds']
+    teams = request.form['teams']
+    duration = request.form['duration']
+    byes = request.form['byes']
+
+    time = int(time)
+    duration = int(duration)
+    rounds = int(rounds)
+    teams = teams.split()
+    byes = int(byes)
+
+    driver.driver(date, time, duration, rounds, teams, byes)
+
+    return app.send_static_file('test.csv')
+
+if __name__ == '__main__':
+    app.run(debug=True)
